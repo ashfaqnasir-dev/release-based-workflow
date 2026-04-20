e75ad7f5702f7479864f6268da2a2292ed633f4b
Ashfaq Nasir <77863129+ashfaqnasir-dev@users.noreply.github.com>
2026-04-20 20:09:17 +0500

    Initial app code for v1.0.0

---
+++ app.js
@@ -0,0 +1,7 @@
+     // Release Based Workflow Demo v1.0.0
+     function greet(name) {
+       return `Hello, ${name}! Welcome to our app.`;
+     }
+     
+     console.log(greet("World"));
+     module.exports = greet;
